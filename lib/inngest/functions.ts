60a88874c6ca10ec0403a3b33fd20cc7790df640
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-04-04 14:09:31 +0200

    add nodemailer

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -1,6 +1,6 @@
 import {inngest} from "@/lib/inngest/client";
 import {NEWS_SUMMARY_EMAIL_PROMPT, PERSONALIZED_WELCOME_EMAIL_PROMPT} from "@/lib/inngest/prompts";
-import {sendNewsSummaryEmail, sendWelcomeEmail} from "@/lib/nordmailer";
+import {sendNewsSummaryEmail, sendWelcomeEmail} from "@/lib/nodemailer";
 import {getAllUsersForNewsEmail} from "@/lib/actions/user.actions";
 import { getWatchlistSymbolsByEmail } from "@/lib/actions/watchlist.actions";
 import { getNews } from "@/lib/actions/finnhub.actions";
